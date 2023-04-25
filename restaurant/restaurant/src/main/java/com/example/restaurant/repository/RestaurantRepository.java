1069feb1fbbcb08c520540381539bee34af3c622
Madhumitha-1404 <120001123+Madhumitha-1404@users.noreply.github.com>
2023-04-25 19:27:09 +0530

    back end

---
+++ restaurant/restaurant/src/main/java/com/example/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,19 @@
+package com.example.restaurant.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.restaurant.model.SwiggyModel;
+
+/*@Repository-is used to indicate that the class provides the mechanism for storage, retrieval, search, update and delete operation on objects  
+JPA Repository is mainly used for managing the data in a Spring Boot Application*/
+/*JpaRepository<Table,Primarykey datatype>
+/*JpaRepository<SwiggyModel,Integer>*/
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<SwiggyModel,Integer> {
+	List<SwiggyModel> findByRestaurantNameContaining(String restaurantName);
+	List<SwiggyModel> findByfoodTypeContaining(String foodType);
+}
